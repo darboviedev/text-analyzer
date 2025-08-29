812da43bb498d411d64900e50356bfc22fc46cdd
Darius Bozkaya <darboviedev@gmail.com>
2025-08-29 08:14:13 +0200

    initial commit backend

---
+++ text-analyzer-backend/src/main/java/com/textanalyzer/TextAnalyzerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.textanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextAnalyzerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextAnalyzerBackendApplication.class, args);
+	}
+
+}
